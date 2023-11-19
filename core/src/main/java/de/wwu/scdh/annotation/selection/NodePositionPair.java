4baaf7db10b7d66f1d7fae6b8cbfaf5b07a7d039
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-19 11:55:03 +0100

    deleted records after they were replaces with Pair<L,R>

--- core/src/main/java/de/wwu/scdh/annotation/selection/NodePositionPair.java
+++
@@ -1,5 +0,0 @@
-package de.wwu.scdh.annotation.selection;
-
-import net.sf.saxon.s9api.XdmNode;
-
-public record NodePositionPair(XdmNode node, int position) {}
--- core/src/main/java/de/wwu/scdh/annotation/selection/XPathPositionPair.java
+++
@@ -1,3 +0,0 @@
-package de.wwu.scdh.annotation.selection;
-
-public record XPathPositionPair(String xpath, int position) {}
