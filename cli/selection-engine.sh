8e346328084ab8bc0eed1eb4ec9837a9cd298996
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-21 00:40:36 +0100

    make shell wrapper work

--- cli/selection-engine.sh
+++ cli/selection-engine.sh
@@ -2,4 +2,4 @@
 
 JAVAOPTS="-Ddebug=true -Dorg.slf4j.simpleLogger.defaultLogLevel=info"
 
-java $JAVAOPTS -cp "${project.build.directory}/lib/*" de.wwu.scdh.annotation.selection.cli.SelectionEngine $@
+java $JAVAOPTS -cp ${project.build.directory}/${project.artifactId}-${project.version}.jar:${project.build.directory}/lib/* de.wwu.scdh.annotation.selection.cli.SelectionEngine $@
