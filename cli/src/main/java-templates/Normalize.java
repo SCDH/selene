361d5e1ddc647a7d37de99da3fb1b10cd3bcbfb5
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-21 10:08:19 +0100

    shorter names for valid parser types, made required parameters required
    This is related to #5

--- cli/src/main/java-templates/Normalize.java
+++ cli/src/main/java-templates/Normalize.java
@@ -43,37 +43,39 @@ public class Normalize implements Callable<Integer> {
 
     @Option(names = { "-p", "--parser" },
 	    paramLabel = "PARSER",
-	    defaultValue = "${DOM_PARSER:DOMParser.XML_PARSER}",
-	    description = "The parser used for reading the RESOURCE. Valid values: ${COMPLETION-CANDIDATES}")
-    DOMParser parser = DOMParser.XML_PARSER;
+	    defaultValue = "${DOM_PARSER:DOMParser.XML}",
+	    description = "The parser used for reading the RESOURCE. Valid values: ${COMPLETION-CANDIDATES}. Defaults to XML.")
+    DOMParser parser = DOMParser.XML;
 
     @Option(names = { "-x", "--xpath" },
+	    required = true,
 	    paramLabel = "XPATH",
 	    description = "the XPath value of an XPath selector")
     String xpath;
 
     @Option(names = { "-c", "--character" },
-	    paramLabel = "CHAR",
-	    description = "the character-scheme position the XPath selector is refined with")
+	    required = true,
+	    paramLabel = "POSITION",
+	    description = "the position the XPath selector is refined with using the character scheme of RFC5147")
     int character;
 
     @Option(names = { "-n", "--normalizer" },
 	    paramLabel = "NORMALIZER",
 	    defaultValue = "${NORMALIZER:DEFAULT_NORMALIZER}",
-	    description = "The normalizer for XPath part of the selector. Valid values: ${COMPLETION-CANDIDATES}")
+	    description = "The normalizer for XPath part of the selector. Valid values: ${COMPLETION-CANDIDATES}. Defaults to FROM_DEEPEST_ID_CLARK")
     Normalizer normalizer = Normalizer.FROM_DEEPEST_ID_CLARK;
 
     @Override
     public Integer call() throws Exception {
 	DOMResource dom;
-	if (parser.equals(DOMParser.XML_PARSER)) {
+	if (parser.equals(DOMParser.XML)) {
 	    try {
 		dom = DOMResource.fromXML(resource, null, PROC);
 	    } catch (Exception e) {
 		System.err.println(e.getMessage());
 		return 1;
 	    }
-	} else if (parser.equals(DOMParser.HTML_PARSER)) {
+	} else if (parser.equals(DOMParser.HTML)) {
 	    try {
 		dom = DOMResource.fromHTML(resource, null, PROC);
 	    } catch (Exception e) {
