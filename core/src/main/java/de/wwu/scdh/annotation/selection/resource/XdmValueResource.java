f2538d3056b5fdd418b53d3c030b75d54cd5916c
Christian Lück <christian.lueck@uni-muenster.de>
2025-11-14 22:13:39 +0100

    deprecate the constructor that leaves a null processor

--- core/src/main/java/de/wwu/scdh/annotation/selection/resource/XdmValueResource.java
+++ core/src/main/java/de/wwu/scdh/annotation/selection/resource/XdmValueResource.java
@@ -5,18 +5,26 @@ import java.net.URI;
 import net.sf.saxon.s9api.XdmValue;
 import net.sf.saxon.s9api.Processor;
 
+/**
+ * The {@link XdmValueResource} is suitable for all kinds of resources
+ * derived from a {@link DOMResource}, even for plain text images.
+ */
 public class XdmValueResource implements S9ApiResource<XdmValue> {
 
     private final URI uri;
     private final XdmValue value;
     private final Processor processor;
 
+    @Deprecated
     public XdmValueResource(URI uri, XdmValue value) {
 	this.uri = uri;
 	this.value = value;
 	this.processor = null;
     }
 
+    /**
+     * Makes a new {@link XdmValueResource}.
+     */
     public XdmValueResource(URI uri, XdmValue value, Processor processor) {
 	this.uri = uri;
 	this.value = value;
