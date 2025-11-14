a34887f4d49f299a8915a65d19cd261e19a110b1
Christian Lück <christian.lueck@uni-muenster.de>
2025-11-14 22:03:59 +0100

    documentation

--- core/src/main/java/de/wwu/scdh/annotation/selection/rewriter/XPathRefinedByRFC5147CharSchemeToTextBackwardMapper.java
+++ core/src/main/java/de/wwu/scdh/annotation/selection/rewriter/XPathRefinedByRFC5147CharSchemeToTextBackwardMapper.java
@@ -22,26 +22,29 @@ import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.XdmItem;
 
 /**
- * Map an {@link XPathRefinedByRFC5147CharScheme} point inside a
- * preimage to the analog point inside the derived plain text image.<P>
+ * Map an {@link RFC5147CharScheme} point into a plain text image to a
+ * {@link XPathRefinedByRFC5147CharScheme} point into the XML
+ * preimage.<P>
  *
  * The mapping involves 3 steps:<P>
  *
- * 1) Descend to the preimage's text node that has the point and save
- * this node and the char scheme position inside it.<P>
+ * 1) In the Xdm Value representation of the image, descend to the
+ * image's text node that has the point and save this node and the
+ * char scheme position inside it.<P>
  *
- * 2) Get the corresponding node of the derived image resource. We now
- * have the image's text node and the char scheme position inside
- * it. Note, that there may be multiple text nodes in the image, that
- * correspond to the same text node in the preimage. So the next step
- * will be applied to each of these pairs of nodes and char scheme
- * positions.<P>
+ * 2) Get the corresponding node of the preimage resource. We now have
+ * the image's text node and the char scheme position inside it. Note,
+ * that there may be one or no text node in the preimage, that
+ * corresponds to the same text node in the image. So the next step
+ * will be applied to maybe a single or no nodes.<P>
  *
- * 3) Calculate the RFC5147 character scheme, i.e. the inter-glyph
- * character position, by summing up the lengths of text nodes
- * preceding the node gotten in step 2 in the image and add the char
- * scheme position gotten in step 2. This sum is the new inter-glyph
- * character position in the plain text image.
+ * 3) Calculate a normalized pair of path expression and char scheme
+ * that is referentially equivalent. This involves getting the path
+ * expression using an XPath that returns a path expression, and then
+ * recalculating the position inside the fragment that is selected by
+ * this path expression. The XPath that returns a path expression is a
+ * configuration feature of this rewriter and thus passed to the
+ * constructor.
  */
 public class XPathRefinedByRFC5147CharSchemeToTextBackwardMapper
     extends XPathRewriterBase
@@ -73,20 +76,17 @@ public class XPathRefinedByRFC5147CharSchemeToTextBackwardMapper
 	(MappedDOMResource preimage,
 	 RFC5147CharScheme position,
 	 RewriterConfig config) throws SelectorException {
-	// step 1: get text node in image where position is located
+	// step 1: get text node in image where position is located we
+	// can use the standard method getTextNodeAtPosition() and
+	// search in `/`, i.e. in the document node.
 	Pair<XdmNode, Integer> imagePair =
 	    getTextNodeAtPosition(preimage.getImage(), "/", position.getChar(), config.getMode());
 	// text nodes are wrapped in trace:text and these wrappers are
 	// mapped to the preimage text nodes. So we have to get the
 	// wrapper node, i.e. the parent.
 	XdmNode textTraceElement = imagePair.getLeft().getParent();
-	System.out.println(imagePair.toString());
-	System.out.println(textTraceElement);
-	System.out.println("constant null: " + null);
-	System.out.println("preimage node: " + preimage.getCorrespondingInPreimage(textTraceElement));
 	// step 2: map text node from image to maybe (one or none) of text node from preimage
 	Optional<XdmNode> preimageNode = preimage.getCorrespondingInPreimage(textTraceElement);
-	System.out.println("preimage node: ");
 	// step 3: rebase each image node as char scheme only: The
 	// charScheme component of the preimagePair is the offset to
 	// the text node in the result. Add the lenght of all text
