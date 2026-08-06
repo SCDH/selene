212243571fa290267b0c0c701e45122dfd07e780
Christian Lück <christian.lueck@uni-muenster.de>
2026-08-06 07:15:38 +0200

    adds test for backward mapping with selector down to element
    This is related to #28.

--- core/src/test/java/de/wwu/scdh/annotation/selection/wadm/TestRewriteAnnotationHtml.java
+++ core/src/test/java/de/wwu/scdh/annotation/selection/wadm/TestRewriteAnnotationHtml.java
@@ -53,6 +53,8 @@ public class TestRewriteAnnotationHtml {
 	public static final String SONG_FW_NOT_IN_IMAGE_JSON = new File(SAMPLE_DIR, "gFwNotInImage.json").toString();
 	public static final String SONG_BW_TO_LEAVE_NOT_IN_PREIMAGE_JSON =
 			new File(SAMPLE_DIR, "gBwHtmlToLeafNotInPreimage.json").toString();
+	public static final String SONG_BW_TO_ELEMENT_NOT_IN_PREIMAGE_JSON =
+			new File(SAMPLE_DIR, "gBwHtmlToElementNotInPreimage.json").toString();
 
 	public static final URI SONG_XML = new File(TEST_DIR, "Gesang.tei.xml").toURI();
 
@@ -676,6 +678,145 @@ public class TestRewriteAnnotationHtml {
 				backwardFactory,
 				normalizerConfig,
 				SONG_BW_TO_LEAVE_NOT_IN_PREIMAGE_JSON,
+				Optional.of("jsonld"));
+		assertEquals(
+				1,
+				model.listStatements((Resource) null, OA.hasTarget, (RDFNode) null)
+						.toSet()
+						.size());
+		Resource specificResource = model.listStatements((Resource) null, OA.hasTarget, (RDFNode) null)
+				.next()
+				.getResource();
+		assertEquals(
+				1,
+				model.listStatements(specificResource, OA.hasSource, (RDFNode) null)
+						.toSet()
+						.size());
+		assertEquals(
+				IRI_SONG,
+				specificResource
+						.getProperty(OA.hasSource)
+						.getObject()
+						.asResource()
+						.toString());
+		Resource rangeSelector = model.listStatements(specificResource, OA.hasSelector, (Resource) null)
+				.next()
+				.getObject()
+				.asResource();
+		// start selector
+		assertEquals(
+				1,
+				model.listStatements(rangeSelector, OA.hasStartSelector, (Resource) null)
+						.toSet()
+						.size(),
+				"has a start selector");
+		Resource startSelector = model.listStatements(rangeSelector, OA.hasStartSelector, (RDFNode) null)
+				.next()
+				.getObject()
+				.asResource();
+		assertEquals(
+				0,
+				model.listStatements(startSelector, RDF.value, (RDFNode) null)
+						.toSet()
+						.size(),
+				"start selector does not have rdf:value");
+		assertEquals(
+				2,
+				model.listStatements(startSelector, RDF.type, (RDFNode) null)
+						.toSet()
+						.size(),
+				"start selector has extra class");
+		assertEquals(
+				1,
+				model.listStatements(startSelector, RDF.type, SEL.BlankedSelector)
+						.toSet()
+						.size(),
+				"start selector is a sel:Null selector");
+		Resource startRefinement = model.listStatements(startSelector, OA.refinedBy, (RDFNode) null)
+				.next()
+				.getObject()
+				.asResource();
+		assertEquals(
+				0,
+				model.listStatements(startRefinement, RDF.value, (RDFNode) null)
+						.toSet()
+						.size(),
+				"start refinement does not have rdf:value");
+		assertEquals(
+				2,
+				model.listStatements(startRefinement, RDF.type, (RDFNode) null)
+						.toSet()
+						.size(),
+				"start refinement has extra class");
+		assertEquals(
+				1,
+				model.listStatements(startRefinement, RDF.type, SEL.BlankedSelector)
+						.toSet()
+						.size(),
+				"start refinement is a sel:Null selector");
+		// end selector
+		assertEquals(
+				1,
+				model.listStatements(rangeSelector, OA.hasEndSelector, (Resource) null)
+						.toSet()
+						.size(),
+				"has a end selector");
+		Resource endSelector = model.listStatements(rangeSelector, OA.hasEndSelector, (RDFNode) null)
+				.next()
+				.getObject()
+				.asResource();
+		assertEquals(
+				0,
+				model.listStatements(endSelector, RDF.value, (RDFNode) null)
+						.toSet()
+						.size(),
+				"end selector does not have rdf:value");
+		assertEquals(
+				2,
+				model.listStatements(endSelector, RDF.type, (RDFNode) null)
+						.toSet()
+						.size(),
+				"end selector has extra class");
+		assertEquals(
+				1,
+				model.listStatements(endSelector, RDF.type, SEL.BlankedSelector)
+						.toSet()
+						.size(),
+				"end selector is a sel:Null selector");
+		Resource endRefinement = model.listStatements(endSelector, OA.refinedBy, (RDFNode) null)
+				.next()
+				.getObject()
+				.asResource();
+		assertEquals(
+				0,
+				model.listStatements(endRefinement, RDF.value, (RDFNode) null)
+						.toSet()
+						.size(),
+				"end refinement does not have rdf:value");
+		assertEquals(
+				2,
+				model.listStatements(endRefinement, RDF.type, (RDFNode) null)
+						.toSet()
+						.size(),
+				"end refinement has extra class");
+		assertEquals(
+				1,
+				model.listStatements(endRefinement, RDF.type, SEL.BlankedSelector)
+						.toSet()
+						.size(),
+				"end refinement is a sel:Null selector");
+	}
+
+	@Test
+	public void testBackwardToElementNotInPreimage() {
+		normalizerConfig = makeConfig("path(.)");
+		model = NormalizeAnnotation.rewrite(
+				songMapped,
+				songIriHtml,
+				songIri,
+				backwardFactory,
+				normalizerConfig,
+				SONG_BW_TO_ELEMENT_NOT_IN_PREIMAGE_JSON,
 				Optional.of("jsonld"));
 		assertEquals(
 				1,
